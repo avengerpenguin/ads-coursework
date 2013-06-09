05cb8a3980cebf09c90508abd2d81a9bc3f3b46e
Ross Fenning <fennir01@MC-S071524.local>
2013-06-09 16:00:45 +0100

    Script should now be generating full postgres import.

--- postgres.py
+++ postgres.py
@@ -1,13 +1,23 @@
 import csv
 import re
-
+from sets import Set
 
 class Category(object):
-    def __init__(self, cid, level, title):
+    def __init__(self, cid, title):
         self.id = cid
-        self.level = level
         self.title = title
 
+    def __eq__(self, other_category):
+        return self.id == other_category.id
+
+    def __hash__(self):
+        return hash(self.id)
+
+class Subcategory(Category):
+    def __init__(self, cid, title, parent):
+        self.id = cid
+        self.title = title
+        self.parent = parent
 
 def sql_field_inject(query_template):
     return query_template.format(
@@ -38,7 +48,7 @@ def replace_all(text, dic):
 
 
 programme_fields = {
-    'pid': 'varchar(10)',
+    'pid': 'varchar(10) PRIMARY KEY',
     'complete_title': 'varchar(256)',
     'media_type': "media_type",
     'masterbrand': 'varchar(20)',
@@ -56,7 +66,7 @@ DROP TABLE IF EXISTS programme;
 DROP TYPE IF EXISTS media_type;
 
 CREATE TABLE category (
-  id    varchar,
+  id    varchar PRIMARY KEY,
   title varchar
 );
 
@@ -73,22 +83,51 @@ print ",\n".join(["\t{}\t{}".format(field_name, field_type)
 print """);
 """
 
+categories = set()
+subcategories = set()
 programme_dicts = []
 
 with open('twoweek_proglist.csv', 'rb') as csvfile:
     for row in csv.DictReader(csvfile):
         row['tags'] = tagstring_to_postgres_array(row['tags'])
-        row['categories'] = [Category(*cat.split(':'))
-                             for cat
-                             in tagstring_to_list(rows['categories'])]
+
+        cat_strings = tagstring_to_list(row['categories'])
+        row['categories'] = []
+        row['subcategories'] = []
+
+        print cat_strings
+        for cat_string in cat_strings:
+            if cat_string:
+                cat_id, cat_level, cat_title = cat_string.split(':')
+
+                if cat_level == '1':
+                    category = Category(cat_id, cat_title)
+                    categories.add(category)
+                    row['categories'].append(category)
+
+                elif cat_level == '2':
+                    subcategory = Subcategory(cat_id, cat_title, category)
+                    subcategories.add(subcategory)
+                    row['subcategories'].append(subcategory)
+
+                else:
+                    raise KeyError('Not expecting category level: ' + cat_level)
+
         programme_dicts.append(row)
 
 
+for category in categories:
+    print "INSERT INTO category (id, title) VALUES ('{}','{}')".format(
+        category.id, category.title)
+
+for subcategory in subcategories:
+    print "INSERT INTO subcategory (id, title, parent) VALUES ('{}','{}','{}')".format(
+        subcategory.id, subcategory.title, subcategory.parent.id)
 
 for programme in programme_dicts:
     print sql_field_inject(
         "INSERT INTO programme ({}) VALUES ({});").format(**programme)
-    
-    break
+    print "INSERT INTO availability_window (start, end, service) VALUES (to_timestamp({}), to_timestamp({}), '{}');".format(
+        int(programme['start_time']), int(programme['end_time']), programme['service'])
 
 print "COMMIT;"
